ae16f2c9ca9a786978f7b0036c0f2506a5f8b18b
Eric Hochwald <erichochwald@Erics-MacBook-Pro.local>
2022-06-20 13:15:03 -0400

    completed and tested the tag routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -21,7 +21,10 @@ router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
   try {
-    const categoryData = await Category.findByPk(req.params.id);
+    const categoryData = await Category.findByPk(req.params.id, {
+      // be sure to include its associated Products
+      include: [{ model:Product }]
+    });
 
     if(!categoryData) {
       res.status(404).json({ message: "No category with this ID"});
