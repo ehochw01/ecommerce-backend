0d263f3741bd2f81a03a6244dc8133bd9fd0a5b9
Eric Hochwald <erichochwald@Erics-MBP.lan>
2022-06-20 00:48:49 -0400

    completed index.js model

--- models/Product.js
+++ models/Product.js
@@ -43,7 +43,7 @@ Product.init(
       type: DataTypes.INTEGER,
     },
     references: {
-      model: 'category', // 'Movies' would also work
+      model: 'category',
       key: 'id'
     }
   },
