4ab274c6d36050aaf60ecc096f4203f52678ef54
Eric Hochwald <erichochwald@Erics-MBP.lan>
2022-06-19 22:21:55 -0400

    set up Product and Category Models

--- models/Product.js
+++ models/Product.js
@@ -9,7 +9,43 @@ class Product extends Model {}
 // set up fields and rules for Product model
 Product.init(
   {
-    // define columns
+    // id: Integer. Doesn't allow null values. Set as primary key.Uses auto increment.
+    id: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    // product_name: String. Doesn't allow null values.
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    // price: Decimal. Doesn't allow null values. Validates that the value is a decimal.
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal: true,
+      }
+    },
+    // stock: Integer. Doesn't allow null values. Set a default value of 10. Validates that the value is numeric.
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric: true,
+      }
+    },
+    // category_id: Integer. References the Category model's id.
+    id: {
+      type: DataTypes.INTEGER,
+    },
+    references: {
+      model: 'category', // 'Movies' would also work
+      key: 'id'
+    }
   },
   {
     sequelize,
